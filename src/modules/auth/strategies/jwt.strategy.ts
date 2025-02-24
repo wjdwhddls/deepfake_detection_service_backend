ee57168f95fa75d5b64063b7c1039000390a7059
thisNorm <kisook2557@gmail.com>
2025-02-24 22:07:03 +0900

    feat : auth 폴더 내에 custom-guards-decorators, dto, strategies 구조 생성

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+
+dotenv.config();
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private userService: UserService) {
+        // [3] Cookie에 있는 JWT 토큰 추출  
+        super({
+            secretOrKey: process.env.JWT_SECRET, // 검증하기 위한 Secret Key  
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // Authorization 헤더에서 JWT 추출
+        });
+    }  // [4] Secret Key로 검증 - 해당 인스턴스가 생성되는 시점 자체가 검증과정
+
+    // [5] JWT에서 사용자 정보 가져오기
+    async validate(payload) {
+        const { email } = payload;
+
+        const user: User = await this.userService.findUserByEmail(email);
+
+        if (!user) {
+            throw new UnauthorizedException('Error');
+        }
+        return user;
+    }
+}
