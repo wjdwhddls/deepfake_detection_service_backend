7070f2d92d8fa3043b0d0c892cc6568e988c4a0c
jjong_0425 <bfpark99@naver.com>
2025-02-24 22:25:33 +0900

    feat : typeORM 설치

--- src/modules/articles/articles.module.ts
+++ src/modules/articles/articles.module.ts
@@ -3,7 +3,7 @@ import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Article } from './entities/article.entity';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from 'src/modules/auth/auth.module';
 
 @Module({
   imports: [
