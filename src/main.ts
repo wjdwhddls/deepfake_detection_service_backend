65c9333593560eda3232ef464feba558b1b921a1
황규범 <kisook2557@gmail.com>
2025-02-25 01:58:09 +0900

    fix: main.ts에서 포트 기본값을 3000으로 설정

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';  
+import { AppModule } from './app.module';  
+import { Logger } from '@nestjs/common';  
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap() {  
+  const app = await NestFactory.create(AppModule);  
 
-  await app.listen(process.env.PORT);
-  Logger.log(`Application Running on Port: ${process.env.PORT}`)
-}
-bootstrap();
+  app.enableCors({  
+    origin: 'http://localhost:4200',  
+    credentials: true,  
+    exposedHeaders: ['Authorization'],  
+  });  
+
+  // 기본값 설정  
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000; // 기본 포트 3000  
+
+  await app.listen(port);  
+  Logger.log(`Application Running on Port: ${port}`);  
+}  
+
+bootstrap();  
